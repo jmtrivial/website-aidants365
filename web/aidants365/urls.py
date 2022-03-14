16189c0b12a37a5d8bed1b3383b2e65f309f9196
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2022-03-14 14:28:35 +0100

    Amélioration de la navigation

--- web/aidants365/urls.py
+++ web/aidants365/urls.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
+
+
 
 urlpatterns = [
+    path('', lambda req: redirect('fichier/')),
     path('fichier/', include('fichier.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
