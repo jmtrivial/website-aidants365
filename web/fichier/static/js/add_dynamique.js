44c3294a809015a89ca6bc3113d499847615078c
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2022-04-12 00:32:47 +0200

    correction d'un bug

--- web/fichier/static/js/add_dynamique.js
+++ web/fichier/static/js/add_dynamique.js
@@ -84,3 +84,29 @@ ajouter.onclick = function() {
 
     return false;
 }
+
+function moveElementToEndOfParent(element) {
+    var parent = element.parent();
+
+    element.detach();
+
+    parent.append(element);
+};
+
+
+function update_field_m2m(element_class) {
+
+    $("#field_wrapper_" + element_class + " ul.select2-selection__rendered").children("li[title]").each(function(i, obj){
+        var element = $("#" + element_class + "_m2m").children("option").filter(function () { return $(this).html() == obj.title; });
+        moveElementToEndOfParent(element);
+    });
+
+    fill_input_m2m(element_class);
+    $("#field_wrapper_" + element_class).trigger('change');
+}
+
+function fill_input_m2m(element_class) {
+    text = $("#" + element_class + "_m2m").val().join();
+    $("#" + element_class).val(text);
+}
+
