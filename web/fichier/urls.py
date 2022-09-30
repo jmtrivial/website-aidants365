8708ce7de1f403e4e5da5c9acb490f0385ad7a1f
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2022-09-30 13:54:42 +0200

    ajout d'un système de cache

--- web/fichier/urls.py
+++ web/fichier/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from . import views
 
+from django.views.decorators.cache import cache_page
 
 app_name = "fichier"
 
+# durée cache par défaut
+dcp = 2 * 60  # 2 mn
+dcf = 15 * 60 # 15 mn
+
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
+    path('', cache_page(dcp)(views.accueil), name='accueil'),
     path('rechercher/', views.rechercher, name='rechercher'),
     path('desk/', views.desk, name='desk'),
     path('desk/<int:id>/', views.document, name='document'),
@@ -35,19 +40,19 @@ urlpatterns = [
     path('theme/<int:id>/', views.index_theme, name='index_theme'),
     path('theme/<int:id1>/<int:id2>/', views.index_theme_detail, name='index_theme_detail'),
     path('theme/<pk>/delete/', views.DeleteThemeView.as_view(), name='theme_delete'),
-    path('motscles/', views.motscles, name='motscles'),
-    path('motscles/alpha/', views.motscles_alpha, name='motscles_alpha'),
-    path('motscles/nuage/', views.motscles_nuage, name='motscles_nuage'),
+    path('motscles/', cache_page(dcf)(views.motscles), name='motscles'),
+    path('motscles/alpha/', cache_page(dcf)(views.motscles_alpha), name='motscles_alpha'),
+    path('motscles/nuage/', cache_page(dcf)(views.motscles_nuage), name='motscles_nuage'),
     path('motcle/<int:id>/', views.index_motcle, name='index_motcle'),
     path('motcle/<int:id1>/<int:id2>/', views.index_motcle_detail, name='index_motcle_detail'),
     path('motcle/<pk>/delete/', views.DeleteMotCleView.as_view(), name='motcle_delete'),
-    path('glossaire/', views.glossaire, name='glossaire'),
+    path('glossaire/', cache_page(dcf)(views.glossaire), name='glossaire'),
     path('glossaire/<int:id>/', views.entree_glossaire, name='entree_glossaire'),
     path('glossaire/search/<str:txt>/', views.recherche_glossaire, name='recherche_glossaire'),
     path('glossaire/<pk>/delete/', views.DeleteEntreeGlossaireView.as_view(), name='entree_glossaire_delete'),
-    path('agenda/', views.agenda_current_month, name='agenda'),
-    path('agenda/<int:year>/', views.agenda_year, name='agenda_year'),
-    path('agenda/<int:year>/<int:month>/', views.agenda_month, name='agenda_month'),
+    path('agenda/', cache_page(dcf)(views.agenda_current_month), name='agenda'),
+    path('agenda/<int:year>/', cache_page(dcf)(views.agenda_year), name='agenda_year'),
+    path('agenda/<int:year>/<int:month>/', cache_page(dcf)(views.agenda_month), name='agenda_month'),
     path('agenda/<int:year>/<int:month>/<int:day>/', views.entree_agenda, name='entree_agenda'),
     path('agenda/entree/<int:id>/', views.entree_agenda_pk, name='entree_agenda_pk'),
     path('agenda/entree/<pk>/delete/', views.DeleteEntreeAgendaView.as_view(), name='entree_agenda_delete'),
