a2afaeb20a7c58a38aa054ff9ae912273a0741dc
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2022-10-01 10:38:29 +0200

    Correction de la gestion du port en mode dev

--- web/aidants365/settings.py
+++ web/aidants365/settings.py
@@ -179,10 +179,17 @@ if DEBUG:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': 'cache:11212',
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+            'LOCATION': 'cache:11211',
+        }
     }
-}
+
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+            'LOCATION': 'cache:11212',
+        }
+    }
